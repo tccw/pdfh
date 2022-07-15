c5066b81a313ca04ff1cccfea516f66b111624b7
Thomas Cowan <thomas.cowan@gmail.com>
2022-07-14 18:01:48 -0700

    build cli skeleton structure and help

---
+++ README.md
@@ -0,0 +1,11 @@
+# PDFH - the PDF hammer
+PDFH is a command-line tool for transforming PDF files.
+
+```
+Details about what is provided will go here 🙂
+```
+
+## The Name
+While writing and picking a name for this tool, I discovered both [QPDF](https://github.com/qpdf/qpdf) and [PDFtk ("tool kit") Server](https://www.pdflabs.com/tools/pdftk-server/), both of which offer much more than this project. I decided that this project is definitely not a tool kit, but maybe it's a single tool: just a hammer. It's not always the perfect tool for the job, but if you only need something simple then a hammer might do.
+
+## Installation Instructions
---
+++ src/main.rs
@@ -0,0 +1,190 @@
+use std::ops::RangeInclusive;
+
+extern crate lopdf;
+
+use clap::{Parser, Subcommand, ArgGroup};
+
+const DEG_MULTIPLE: u16 = 90;
+
+#[derive(Parser, Debug)]
+#[clap(name = "tpdf")]
+#[clap(author = "Thomas C.")]
+#[clap(version = "0.1.0")]
+#[clap[about = "A tool for PDF manipulation"]]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    #[clap(arg_required_else_help = false)]
+    /// Merges pdfs into a single file
+    Merge {
+        #[clap(required = true, parse(from_os_str))]
+        infiles: Vec<std::path::PathBuf>,
+        #[clap(required = true, parse(from_os_str))]
+        outfile: std::path::PathBuf,
+        #[clap(short, long)]
+        compress: bool
+    },
+    #[clap(arg_required_else_help = false)]
+    /// Splits each page of a pdf into a separate pdf
+    Split {
+        #[clap(required = true, parse(from_os_str))]
+        infile: std::path::PathBuf,
+        #[clap(required = true, parse(from_os_str))]
+        outfile: std::path::PathBuf,
+        #[clap(short, long)]
+        compress: bool
+    },
+    #[clap(arg_required_else_help = false)]
+    /// Duplicates a pdf n times and saves the duplicates into a single file
+    Dupe {
+        #[clap(required = true, parse(from_os_str))]
+        infile: std::path::PathBuf,
+        #[clap(required = true, parse(from_os_str))]
+        outfile: std::path::PathBuf,
+        #[clap(required = true, short, long)]
+        // Number of times to duplicate
+        num: u16,
+        #[clap(short, long)]
+        compress: bool
+    },
+    #[clap(arg_required_else_help = false)]
+    /// Rotate an entire document, or select pages
+    Rotate {
+        #[clap(required = true, parse(from_os_str))]
+        infile: std::path::PathBuf,
+        /// Modified inplace if not provided
+        #[clap(required = false, parse(from_os_str))]
+        outfile: std::path::PathBuf,
+        #[clap(required=true, value_parser = degree_in_range, short, long)]
+        /// Positive values are CW, negative are CCW rotation. Multipules of 90.
+        degrees: u16,
+        #[clap(required=false, short, long)]
+        /// List of space separated page numbers. All pages if not provided.
+        pages: Vec<u16>,
+        #[clap(short, long)]
+        compress: bool
+    },
+    #[clap(arg_required_else_help = false)]
+    #[clap(group(
+        ArgGroup::new("dels")
+            .required(false)
+            .args(&["pages", "every"])
+        ))]
+    /// Delete pages from a pdf document. 
+    /// A list of space separated pages or --every ith page
+    Delete {
+        #[clap(required = true, parse(from_os_str))]
+        infile: std::path::PathBuf,
+        #[clap(required = false, parse(from_os_str))]
+        /// Modified inplace if not provided
+        outfile: Option<std::path::PathBuf>,
+        #[clap(group = "dels", short, long,  multiple=true, value_parser)]
+        /// List of space separated page numbers
+        pages: Option<Vec<u32>>,
+        #[clap(group = "dels", short, long, value_parser)]
+        /// Delete every ith page
+        every: Option<u32>,
+        #[clap(required=false, long)]
+        /// Negates the deletion operation, i.e. keep only the listed pages. 
+        /// Used with --every, it will keep every ith page rather than delete it.
+        negate: bool,
+        #[clap(short, long)]
+        compress: bool
+
+    },
+    #[clap(arg_required_else_help = false)]
+    /// Reverse the order of a PDF
+    Reverse {
+        #[clap(required = true, parse(from_os_str))]
+        infile: std::path::PathBuf,
+        #[clap(required = false, parse(from_os_str))]
+        /// Modified inplace if not provided
+        outfile: Option<std::path::PathBuf>,
+    },
+    #[clap(arg_required_else_help = false)]
+    #[clap(group(
+        ArgGroup::new("extract")
+            .required(false)
+            .args(&["pages", "every"])
+        ))]
+    /// Extract specitic pages from a PDF
+    Extract {
+        #[clap(required = true, parse(from_os_str))]
+        infile: std::path::PathBuf,
+        #[clap(required = true, parse(from_os_str))]
+        /// Modified inplace if not provided
+        outfile: std::path::PathBuf,
+        #[clap(group = "extract", short, long,  multiple=true, value_parser)]
+        /// List of space separated page numbers
+        pages: Option<Vec<u32>>,
+        #[clap(group = "extract", short, long, value_parser)]
+        /// Delete every ith page
+        every: Option<u32>,
+    }
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Merge { mut infiles, outfile, compress } => {
+            // TODO
+            println!("Not Implemented");
+        }
+        Commands::Split { infile, outfile, compress} => {
+            // TODO
+            println!("Not Implemented");
+        }
+        Commands::Dupe { infile, outfile, num, compress} => {
+            // TODO
+            println!("Not Implemented");
+        }
+        Commands::Rotate { infile,
+                           outfile, 
+                           degrees, 
+                           pages,
+                           compress } => {
+            // TODO
+            println!("Not Implemented");
+        },
+        Commands::Delete { infile, 
+                           outfile, 
+                           pages, 
+                           every, 
+                           negate,
+                           compress } => {
+            // TODO
+            println!("Not Implemented");
+        },
+        Commands::Reverse { infile, outfile } => {
+            // TODO
+            println!("Not Implemented");
+        },
+        Commands::Extract { infile, 
+                            outfile, 
+                            pages, 
+                            every } => {
+            // TODO
+            println!("Not Implemented");
+        }
+    }    
+}
+
+
+fn degree_in_range(s: &str) -> Result<u16, String> {
+    let degree: u16 = s
+        .parse()
+        .map_err(|_| format!("`{}` is not an integer", s))?;
+    if degree % DEG_MULTIPLE == 0 {
+        Ok(degree)
+    } else {
+        Err(format!(
+            "degrees must be a multiple of {}",
+            DEG_MULTIPLE
+        ))
+    }
+}
