0dd2f7d369db731022f6b8ab9f8b22e5620f4cf0
Thomas Cowan <thomas.cowan@gmail.com>
2022-07-21 21:57:22 -0700

    feat: initial merge impl
    some issues remain in multi-Pages object edge cases

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,41 @@
-use std::{collections::{HashSet, BTreeMap}, path::{PathBuf}};
+use std::{
+    collections::{HashSet, BTreeMap},
+    fs,
+    path::{PathBuf}
+};
 use lopdf::{Document, Object, ObjectId};
+
+const VERSION: &str = "1.5";
+
+/// Creates a silgle PDF containing all passed infiles, or all PDFs in passed directories
+/// 
+/// # Arguments
+/// 
+/// * `infiles` - a vector of PathBuf which could include directories or files
+/// * `outfile` - a PathBuf representing the location to save the merged file to
+/// * `compress` - a boolean flag to compress the outfile file before saving
+/// 
+pub fn merge(infiles: &Vec<PathBuf>, outfile: PathBuf, compress: bool) {
+    // make vector of Document data structures
+    let mut documents: Vec<Document> = Vec::new();
+    let mut doc: Document;
+    let mut document = Document::with_version(VERSION);
+
+    let files = expand_dirs_if_necessary(infiles);
+
+    for fname in files {
+        // TODO: should not panic on I/O error as this is common. Handle better for user.
+        doc = Document::load(&fname).expect("failed to open PDF");
+        documents.push(doc);
+    }
+
+    merge_documents(documents, &mut document);
+
+    if compress { document.compress(); }
+
+    // Save the merged PDF
+    save_pdf(&mut document, outfile);
+}
 
 /// Deletes the pages listed in --pages, or deletes every --every page in a PDF
 /// 
@@ -274,6 +310,217 @@ fn rotate_all_pages(doc: &mut Document, degrees: i32) {
                 
                 dict.set("Rotate", degrees);
                 doc.objects.insert(object_id, Object::Dictionary(dict));
+            }
+        }
+    }
+}
+
+// check if any of the entries are directories, if they are, expand the vector to include
+// all PDFs in the directory (do not search subdirs)
+fn expand_dirs_if_necessary(infiles: &Vec<PathBuf>) -> Vec<PathBuf> {
+    let mut dir_pdf_files: Vec<PathBuf>;
+    let mut expanded = Vec::with_capacity(infiles.len());
+
+    for path in infiles {
+        if path.is_dir() {
+            dir_pdf_files = get_files_from_dir(path).expect("failed to get files from directory");
+            expanded.append(&mut dir_pdf_files);
+        } else  {
+            expanded.push(path.to_path_buf());
+        }
+    }
+
+    return expanded;
+}
+
+// https://stackoverflow.com/questions/58062887/filtering-files-or-directories-discovered-with-fsread-dir
+fn get_files_from_dir(dir: &PathBuf) -> Result<Vec<PathBuf>, std::io::Error> {
+    Ok(fs::read_dir(dir)?
+        .into_iter()
+        .filter(|result| result.is_ok()) // filter to non Err results
+        .map(|result| result.unwrap().path()) // turn valid DirEntries into PathBufs
+        .filter(|result| 
+            result.is_file() && 
+            result.extension() != None && 
+            (result.extension().unwrap() == "pdf" || result.extension().unwrap() == "PDF")) // filter to only files
+        .collect()
+    )
+}
+
+// this is almost unmodified from the examples in the lopdf README https://github.com/J-F-Liu/lopdf
+// TODO: consider refactoring
+// FIXME: this is broken for files with multiple Pages objects (I think)
+fn merge_documents(documents: Vec<Document>, outdoc: &mut Document) {
+    // Define a starting max_id (will be used as start index for object_ids)
+    let mut max_id = 1;
+    // let mut pagenum = 1;
+    // Collect all Documents Objects grouped by a map
+    let mut documents_pages = BTreeMap::new();
+    let mut documents_objects = BTreeMap::new();
+
+    for mut doc in documents {
+        // let mut first = false;
+
+        // renumber the current doc starting with the current max_id
+        doc.renumber_objects_with(max_id);
+        // sets the new max_id to the id of the last page of the current doc + 1 so that the next doc starts in the correct location
+        max_id = doc.max_id + 1; 
+
+        // extend the documents_pages with a BTreeMap of ObjectId and Object which is a enum of Object types
+        // An object can be:
+        /*
+            pub enum Object {
+                Null,
+                Boolean(bool),
+                Integer(i64),
+                Real(f64),
+                Name(Vec<u8>),
+                String(Vec<u8>, StringFormat),
+                Array(Vec<Object>),
+                Dictionary(Dictionary),
+                Stream(Stream),
+                Reference(ObjectId),
+            }
+        */
+        documents_pages.extend(
+            doc
+                    .get_pages()
+                    .into_iter()
+                    .map(|(_, object_id)| (object_id, doc.get_object(object_id).unwrap().to_owned(),))
+                    .collect::<BTreeMap<ObjectId, Object>>(),
+        );
+
+        // add all the objects from each document to a collection
+        documents_objects.extend(doc.objects);
+    }
+
+    // Catalog and Pages are mandatory 
+    let mut catalog_object: Option<(ObjectId, Object)> = None;
+    let mut pages_object: Option<(ObjectId, Object)> = None;
+
+    // Process all objects except "Page" type
+    for (object_id, object) in documents_objects.iter() {
+        // We have to ignore "Page" (as are processed later), "Outlines" and "Outline" objects
+        // All other objects should be collected and inserted into the main Document
+        match object.type_name().unwrap_or("") {
+            "Catalog" => {
+                // Collect a first "Catalog" object and use it for the future "Pages"
+                catalog_object = Some((
+                    if let Some((id, _)) = catalog_object {
+                        id
+                    } else {
+                        *object_id
+                    },
+                    object.clone(),
+                ));
+            }
+            "Pages" => {
+                // Collect and update a first "Pages" object and use it for the future "Catalog"
+                // We have also to merge all dictionaries of the old and the new "Pages" object
+                if let Ok(dictionary) = object.as_dict() {
+                    let mut dictionary = dictionary.clone();
+                    if let Some((_, ref object)) = pages_object {
+                        if let Ok(old_dictionary) = object.as_dict() {
+                            dictionary.extend(old_dictionary);
+                        }
+                    }
+
+                    pages_object = Some((
+                        if let Some((id, _)) = pages_object {
+                            id
+                        } else {
+                            *object_id
+                        },
+                        Object::Dictionary(dictionary),
+                    ));
+                }
+            }
+            "Page" => {}     // Ignored, processed later and separately
+            "Outlines" => {} // Ignored, not supported yet
+            "Outline" => {}  // Ignored, not supported yet
+            _ => {
+                outdoc.objects.insert(*object_id, object.clone());
+            }
+        }
+    }
+
+    // If no "Pages" found abort
+    if pages_object.is_none() {
+        println!("Pages root not found.");
+
+        return;
+    }
+
+    // Iter over all "Page" and collect with the parent "Pages" created before
+    for (object_id, object) in documents_pages.iter() {
+        if let Ok(dictionary) = object.as_dict() {
+            let mut dictionary = dictionary.clone();
+            dictionary.set("Parent", pages_object.as_ref().unwrap().0);
+
+            outdoc
+                    .objects
+                    .insert(*object_id, Object::Dictionary(dictionary));
+        }
+    }
+
+    // If no "Catalog" found abort
+    if catalog_object.is_none() {
+        println!("Catalog root not found.");
+
+        return;
+    }
+
+    let catalog_object = catalog_object.unwrap();
+    let pages_object = pages_object.unwrap();
+
+    // Build a new "Pages" with updated fields
+    if let Ok(dictionary) = pages_object.1.as_dict() {
+        let mut dictionary = dictionary.clone();
+
+        // Set new pages count
+        dictionary.set("Count", documents_pages.len() as u32);
+
+        // Set new "Kids" list (collected from documents pages) for "Pages"
+        dictionary.set(
+            "Kids",
+            documents_pages
+                    .into_iter()
+                    .map(|(object_id, _)| Object::Reference(object_id))
+                    .collect::<Vec<_>>(),
+        );
+
+        outdoc
+                .objects
+                .insert(pages_object.0, Object::Dictionary(dictionary));
+    }
+
+    // Build a new "Catalog" with updated fields
+    if let Ok(dictionary) = catalog_object.1.as_dict() {
+        let mut dictionary = dictionary.clone();
+        dictionary.set("Pages", pages_object.0);
+        dictionary.remove(b"Outlines"); // Outlines not supported in merged PDFs
+
+        outdoc
+                .objects
+                .insert(catalog_object.0, Object::Dictionary(dictionary));
+    }
+
+    outdoc.trailer.set("Root", catalog_object.0);
+
+    // Update the max internal ID as wasn't updated before due to direct objects insertion
+    outdoc.max_id = outdoc.objects.len() as u32;
+
+    // Reorder all new Document objects
+    outdoc.renumber_objects();
+
+     //Set any Bookmarks to the First child if they are not set to a page
+    outdoc.adjust_zero_pages();
+
+    //Set all bookmarks to the PDF Object tree then set the Outlines to the Bookmark content map.
+    if let Some(n) = outdoc.build_outline() {
+        if let Ok(x) = outdoc.get_object_mut(catalog_object.0) {
+            if let Object::Dictionary(ref mut dict) = x {
+                dict.set("Outlines", Object::Reference(n));
             }
         }
     }
